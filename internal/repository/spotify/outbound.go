63a22678b35ac515958b7e908f9f8c4c5dfb2efd
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-06 04:22:06 +0700

    feat(spotify.api/search): Add Spotify outbound struct and constructor for API interaction

---
+++ internal/repository/spotify/outbound.go
@@ -0,0 +1,22 @@
+package spotify
+
+import (
+	"github.com/mdafaardiansyah/musicacu-backend/internal/configs"
+	"github.com/mdafaardiansyah/musicacu-backend/pkg/httpclient"
+	"time"
+)
+
+type outbound struct {
+	cfg         *configs.Config
+	client      httpclient.HTTPClient
+	AccessToken string
+	TokenType   string
+	ExpiredAt   time.Time
+}
+
+func NewSpotifyOutbound(cfg *configs.Config, client httpclient.HTTPClient) *outbound {
+	return &outbound{
+		cfg:    cfg,
+		client: client,
+	}
+}
