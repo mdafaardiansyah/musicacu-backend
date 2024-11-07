fc45c655fd699350acaea386afbcd8e18eb50939
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-07 12:28:26 +0700

    feat(trackactivities/like-dislike): make Handler-level for like and dislike activities feature

--- internal/handler/tracks/handler.go
+++ internal/handler/tracks/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/mdafaardiansyah/musicacu-backend/internal/middleware"
 	"github.com/mdafaardiansyah/musicacu-backend/internal/models/spotify"
+	"github.com/mdafaardiansyah/musicacu-backend/internal/models/trackactivities"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,7 @@ import (
 //go:generate mockgen -source=handler.go -destination=handler_mock_test.go -package=tracks
 type service interface {
 	Search(ctx context.Context, query string, pageSize, pageIndex int, userID uint) (*spotify.SearchResponse, error)
+	UpsertTrackActivities(ctx context.Context, userID uint, request trackactivities.TrackActivityRequest) error
 }
 
 type Handler struct {
@@ -29,4 +31,5 @@ func (h *Handler) RegisterRoute() {
 	route := h.Group("/tracks")
 	route.Use(middleware.AuthMiddleware())
 	route.GET("/search", h.Search)
+	route.POST("/track-activity", h.UpsertTrackActivities)
 }
