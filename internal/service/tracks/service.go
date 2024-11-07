7e991dfb74b9c586eb8a76dc0e9d90eca3f90942
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-07 12:17:50 +0700

    feat(trackactivities/like-dislike): make Service-level for like and dislike activities feature

--- internal/service/tracks/service.go
+++ internal/service/tracks/service.go
@@ -2,6 +2,7 @@ package tracks
 
 import (
 	"context"
+	"github.com/mdafaardiansyah/musicacu-backend/internal/models/trackactivities"
 	"github.com/mdafaardiansyah/musicacu-backend/internal/repository/spotify"
 )
 
@@ -10,10 +11,18 @@ type spotifyOutbound interface {
 	Search(ctx context.Context, query string, limit, offset int) (*spotify.SpotifySearchResponse, error)
 }
 
-type service struct {
-	spotifyOutbound spotifyOutbound
+type trackActivitiesRepository interface {
+	Create(ctx context.Context, model trackactivities.TrackActivity) error
+	Update(ctx context.Context, model trackactivities.TrackActivity) error
+	Get(ctx context.Context, userID uint, spotifyID string) (*trackactivities.TrackActivity, error)
+	GetBulkSpotifyIDs(ctx context.Context, userID uint, spotifyIDs []string) (map[string]trackactivities.TrackActivity, error)
 }
 
-func NewService(spotifyOutbound spotifyOutbound) *service {
-	return &service{spotifyOutbound: spotifyOutbound}
+type service struct {
+	spotifyOutbound     spotifyOutbound
+	trackActivitiesRepo trackActivitiesRepository
+}
+
+func NewService(spotifyOutbound spotifyOutbound, trackActivitiesRepo trackActivitiesRepository) *service {
+	return &service{spotifyOutbound: spotifyOutbound, trackActivitiesRepo: trackActivitiesRepo}
 }
